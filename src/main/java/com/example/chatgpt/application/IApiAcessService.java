1af6b17898c0d09200af7fa8b99213e7d634730f
liuyang <2391461132@qq.com>
2024-03-03 19:38:01 +0800

    使用Shiro鉴权

---
+++ src/main/java/com/example/chatgpt/application/IApiAcessService.java
@@ -0,0 +1,9 @@
+package com.example.chatgpt.application;
+/**
+ * @author LYFX
+ * @description 暂时占位
+ * @github https://github.com/*********
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn
+ **/
+public interface IApiAcessService {
+}
---
+++ src/main/java/com/example/chatgpt/infrastracture/common/Constants.java
@@ -0,0 +1,8 @@
+package com.example.chatgpt.infrastracture.common;
+
+/**
+ * @author LYFX
+ * @description 公共基础对象
+ */
+public class Constants {
+}
