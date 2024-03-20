aaea3620dc53b42facd9e501db5e27c9d66eeaf2
liuyang <2391461132@qq.com>
2024-03-20 23:46:15 +0800

    feat: 添加ChatGLMConfig配置

---
+++ chatgpt-data-app/src/main/java/org/example/chatgpt/data/config/ChatGLMSDKConfigProperties.java
@@ -0,0 +1,16 @@
+package org.example.chatgpt.data.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @description 将应用程序的配置属性绑定到java对象中方便进行管理
+ */
+@Data
+@ConfigurationProperties(prefix = "chatglm.sdk.config", ignoreInvalidFields = true)
+public class ChatGLMSDKConfigProperties {
+    private boolean enable;
+    /**转发地址**/
+    private String apiHost;
+    private String apiSecretKey;
+}
